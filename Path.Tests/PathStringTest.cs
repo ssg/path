630af3dc1c262ce3f34c5f259c3f92a8374a24d3
Sedat Kapanoglu <sedat@kapanoglu.com>
2024-09-25 21:08:47 -0700

    fix tests

--- Path.Tests/PathStringTest.cs
+++ Path.Tests/PathStringTest.cs
@@ -1,11 +1,22 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework;
 
 namespace PathCli.Tests;
 
+#if _WINDOWS
 [TestFixture]
 public class PathStringTest
 {
 #pragma warning disable CA1861 // Avoid constant arrays as arguments
+    private static string osPath(string path)
+    {
+#if !_WINDOWS
+        return path.Replace(';', Path.PathSeparator);
+#else
+        return path;
+#endif
+    }
+
     [Test]
     [TestCase("a;b", new[] { "a", "b" })]
     [TestCase("\"a\";\"b\"", new[] { "a", "b" })]
@@ -13,6 +24,11 @@ public class PathStringTest
     [TestCase("\"a;b\";\"c;d\"", new[] { "a;b", "c;d" })]
     public void ctor_validPathString(string input, string[] output)
     {
+        input = osPath(input);
+        for (int n = 0; n < output.Length; n++)
+        {
+            output[n] = osPath(output[n]);
+        }
         var path = new PathString(input);
         Assert.That(path.Items, Is.EqualTo(output));
     }
@@ -23,6 +39,8 @@ public class PathStringTest
     [TestCase("a;b;c", "b", 2, "a;c;b")]
     public void MoveTo_ValidInput_Succeeds(string input, string dir, int destIndex, string output)
     {
+        input = osPath(input);
+        output = osPath(output);
         var path = new PathString(input);
         Assert.That(path.MoveTo(dir, destIndex), Is.True);
         Assert.That(path.ToString, Is.EqualTo(output));
@@ -34,6 +52,7 @@ public class PathStringTest
     [TestCase("a;b;c", "b", 3)]
     public void MoveTo_InvalidArguments_Fails(string input, string dir, int destIndex)
     {
+        input = osPath(input);
         var path = new PathString(input);
         Assert.That(path.MoveTo(dir, destIndex), Is.False);
     }
@@ -44,6 +63,8 @@ public class PathStringTest
     [TestCase("a;b;c", "b", "c", "a;b;c")]
     public void MoveBefore_ValidArguments_Succeeds(string input, string dir, string destDir, string output)
     {
+        input = osPath(input);
+        output = osPath(output);
         var path = new PathString(input);
         Assert.That(path.MoveBefore(dir, destDir), Is.True);
         Assert.That(path.ToString(), Is.EqualTo(output));
@@ -56,6 +77,8 @@ public class PathStringTest
     [TestCase("a;b;c;d", "b", "c", "a;c;b;d")]
     public void MoveAfter_ValidArguments_Succeeds(string input, string dir, string destDir, string output)
     {
+        input = osPath(input);
+        output = osPath(output);
         var path = new PathString(input);
         Assert.That(path.MoveAfter(dir, destDir), Is.True);
         Assert.That(path.ToString(), Is.EqualTo(output));
@@ -70,8 +93,11 @@ public class PathStringTest
     [TestCase("a;\"b;c\";d")]
     public void ToString_PreservesTheString(string input)
     {
+        input = osPath(input);
         var path = new PathString(input);
         Assert.That(path.ToString(), Is.EqualTo(input));
     }
 #pragma warning restore CA1861 // Avoid constant arrays as arguments
 }
+
+#endif
