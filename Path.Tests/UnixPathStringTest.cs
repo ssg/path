7b816d4038e5e0d60600fb967d3e2aa68c45b86c
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-09-20 22:16:53 -0700

    add more tests for PathString

--- Path.Tests/UnixPathStringTest.cs
+++ Path.Tests/UnixPathStringTest.cs
@@ -73,4 +73,88 @@ public class UnixPathStringTest
         var path = new UnixPathString(input);
         Assert.That(path.ToString(), Is.EqualTo(input));
     }
+
+    // Add method tests
+    [Test]
+    [TestCase("a:b", "c", true, "a:b:c")]
+    [TestCase("a:b", "a", false, "a:b")] // Duplicate item
+    [TestCase("a:b", "A", true, "a:b:A")] // Case sensitive - different from Windows
+    [TestCase("", "a", true, "a")] // Add to empty path
+    [TestCase("a:b", " c ", true, "a:b:c")] // Trimming whitespace
+    [TestCase("a:b", "/usr/local/bin", true, "a:b:/usr/local/bin")] // Typical Unix path
+    public void Add_ValidScenarios_ReturnsExpectedResult(string initial, string itemToAdd, bool expectedResult, string expectedPath)
+    {
+        var path = new UnixPathString(initial);
+        var result = path.Add(itemToAdd);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+        Assert.That(path.ToString(), Is.EqualTo(expectedPath));
+    }
+
+    [Test]
+    [TestCase("", "  ")]
+    [TestCase("a:b", "  ")]
+    public void Add_WhitespaceOnly_ReturnsTrue(string initial, string itemToAdd)
+    {
+        var path = new UnixPathString(initial);
+        var result = path.Add(itemToAdd);
+        
+        Assert.That(result, Is.True);
+        Assert.That(path.Items.Last(), Is.EqualTo(""));
+    }
+
+    [Test]
+    public void Add_ItemContainsDelimiter_ThrowsException()
+    {
+        var path = new UnixPathString("a:b");
+        
+        Assert.Throws<InvalidOperationException>(() => path.Add("c:d"));
+    }
+
+    [Test]
+    public void Add_ItemContainsDelimiterInMiddle_ThrowsException()
+    {
+        var path = new UnixPathString("/usr/bin:/usr/local/bin");
+        
+        Assert.Throws<InvalidOperationException>(() => path.Add("/path:with:colons"));
+    }
+
+    // RemoveAll method tests
+    [Test]
+    [TestCase("a:b:c", "b", true, "a:c")]
+    [TestCase("a:b:c:b", "b", true, "a:c")] // Multiple occurrences
+    [TestCase("a:b:c", "d", false, "a:b:c")] // Item not found
+    [TestCase("a:b:c", "B", false, "a:b:c")] // Case sensitive - different from Windows
+    [TestCase("", "a", false, "")] // Empty path
+    [TestCase("a", "a", true, "")] // Remove only item
+    [TestCase("a:a:a", "a", true, "")] // Remove all occurrences of same item
+    [TestCase("/usr/bin:/usr/local/bin:/opt/bin", "/usr/bin", true, "/usr/local/bin:/opt/bin")] // Unix paths
+    public void RemoveAll_ValidScenarios_ReturnsExpectedResult(string initial, string itemToRemove, bool expectedResult, string expectedPath)
+    {
+        var path = new UnixPathString(initial);
+        var result = path.RemoveAll(itemToRemove);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+        Assert.That(path.ToString(), Is.EqualTo(expectedPath));
+    }
+
+    [Test]
+    public void RemoveAll_MultipleOccurrencesAtDifferentPositions_RemovesAll()
+    {
+        var path = new UnixPathString("a:b:a:c:a:d");
+        var result = path.RemoveAll("a");
+        
+        Assert.That(result, Is.True);
+        Assert.That(path.ToString(), Is.EqualTo("b:c:d"));
+    }
+
+    [Test]
+    public void RemoveAll_CaseSensitive_OnlyRemovesExactMatches()
+    {
+        var path = new UnixPathString("a:A:b:a:B");
+        var result = path.RemoveAll("a");
+        
+        Assert.That(result, Is.True);
+        Assert.That(path.ToString(), Is.EqualTo("A:b:B"));
+    }
 }
--- Path.Tests/WindowsPathStringTest.cs
+++ Path.Tests/WindowsPathStringTest.cs
@@ -72,4 +72,62 @@ public class WindowsPathStringTest
         var path = new WindowsPathString(input);
         Assert.That(path.ToString(), Is.EqualTo(input));
     }
+
+    // Add method tests
+    [Test]
+    [TestCase("a;b", "c", true, "a;b;c")]
+    [TestCase("a;b", "a", false, "a;b")] // Duplicate item
+    [TestCase("a;b", "A", false, "a;b")] // Case insensitive duplicate for Windows
+    [TestCase("", "a", true, "a")]
+    [TestCase("a;b", " c ", true, "a;b;c")] // Trimming whitespace
+    [TestCase("a;b", "\"c\"", true, "a;b;c")] // Quoted item gets unquoted
+    [TestCase("a;b", "c;d", true, "a;b;\"c;d\"")] // Item with delimiter gets quoted automatically
+    public void Add_ValidScenarios_ReturnsExpectedResult(string initial, string itemToAdd, bool expectedResult, string expectedPath)
+    {
+        var path = new WindowsPathString(initial);
+        var result = path.Add(itemToAdd);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+        Assert.That(path.ToString(), Is.EqualTo(expectedPath));
+    }
+
+    [Test]
+    [TestCase("", "  ")]
+    [TestCase("a;b", "  ")]
+    public void Add_WhitespaceOnly_ReturnsTrue(string initial, string itemToAdd)
+    {
+        var path = new WindowsPathString(initial);
+        var result = path.Add(itemToAdd);
+        
+        Assert.That(result, Is.True);
+        Assert.That(path.Items.Last(), Is.EqualTo(""));
+    }
+
+    // RemoveAll method tests
+    [Test]
+    [TestCase("a;b;c", "b", true, "a;c")]
+    [TestCase("a;b;c;b", "b", true, "a;c")] // Multiple occurrences
+    [TestCase("a;b;c", "d", false, "a;b;c")] // Item not found
+    [TestCase("a;b;c", "B", true, "a;c")] // Case insensitive for Windows
+    [TestCase("", "a", false, "")] // Empty path
+    [TestCase("a", "a", true, "")] // Remove only item
+    [TestCase("a;a;a", "a", true, "")] // Remove all occurrences of same item
+    public void RemoveAll_ValidScenarios_ReturnsExpectedResult(string initial, string itemToRemove, bool expectedResult, string expectedPath)
+    {
+        var path = new WindowsPathString(initial);
+        var result = path.RemoveAll(itemToRemove);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+        Assert.That(path.ToString(), Is.EqualTo(expectedPath));
+    }
+
+    [Test]
+    public void RemoveAll_MultipleOccurrencesAtDifferentPositions_RemovesAll()
+    {
+        var path = new WindowsPathString("a;b;a;c;a;d");
+        var result = path.RemoveAll("a");
+        
+        Assert.That(result, Is.True);
+        Assert.That(path.ToString(), Is.EqualTo("b;c;d"));
+    }
 }
